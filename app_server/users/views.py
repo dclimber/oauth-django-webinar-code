5247dcaaf9a8a0d0752eeae0b49aaeadbdfc0e90
Dastan Abd <dastand.climber@gmail.com>
2021-03-11 17:15:23 +0600

    feat: Add views

---
+++ app_server/users/views.py
@@ -0,0 +1,109 @@
+import logging
+
+import requests
+from decouple import config
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from .serializers import CreateUserSerializer
+
+CLIENT_ID = config('CLIENT_ID')
+CLIENT_SECRET = config('CLIENT_SECRET')
+OAUTH_SERVER_URL = config('OAUTH_SERVER_URL')
+
+logger = logging.getLogger('django.server')
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register(request):
+    '''
+    Registers user to the server. Input should be in the format:
+    {"username": "username", "password": "1234abcd"}
+    '''
+    # Put the data from the request into the serializer 
+    serializer = CreateUserSerializer(data=request.data)
+
+    # Validate the data
+    if serializer.is_valid():
+        # If it is valid, save the data (creates a user).
+        serializer.save()
+        logger.info('user created')
+        # Then we get a token for the created user.
+        # This could be done differently
+        r = requests.post(
+            f'{OAUTH_SERVER_URL}/o/token/',
+            data={
+                'grant_type': 'password',
+                'username': request.data['username'],
+                'password': request.data['password'],
+                'client_id': CLIENT_ID,
+                'client_secret': CLIENT_SECRET,
+            },
+        )
+        return Response(r.json())
+    logger.info('Serializer is not valid')
+    return Response(serializer.errors)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def token(request):
+    """
+    Gets tokens with username and password. Input should be in the format:
+    {"username": "username", "password": "1234abcd"}
+    """
+    r = requests.post(
+        f'{OAUTH_SERVER_URL}/o/token/',
+        data={
+            'grant_type': 'password',
+            'username': request.data['username'],
+            'password': request.data['password'],
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+        },
+    )
+    return Response(r.json())
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def refresh_token(request):
+    """
+    Registers user to the server. Input should be in the format:
+    {"refresh_token": "<token>"}
+    """
+    r = requests.post(
+        f'{OAUTH_SERVER_URL}/o/token/',
+        data={
+            'grant_type': 'refresh_token',
+            'refresh_token': request.data['refresh_token'],
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+        },
+    )
+    return Response(r.json())
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def revoke_token(request):
+    """
+    Method to revoke tokens.
+    {"token": "<token>"}
+    """
+    r = requests.post(
+        f'{OAUTH_SERVER_URL}/o/revoke_token/',
+        data={
+            'token': request.data['token'],
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+        },
+    )
+    # If it goes well return sucess message (would be empty otherwise)
+    if r.status_code == requests.codes.ok:
+        return Response({'message': 'token revoked'}, r.status_code)
+
+    # Return the error if it goes badly
+    return Response(r.json(), r.status_code)
