0f50278b68d319ff0c366b0af8dbcc854baa41c9
Dastan Abd <dastand.climber@gmail.com>
2021-03-11 17:58:23 +0600

    feat: Add poem model

--- auth_server/poems/models.py
+++ auth_server/poems/models.py
@@ -21,3 +21,20 @@ class Poet(models.Model):
             f'{self.first_name} {self.last_name}'
             if self.last_name is not None else f'{self.first_name}'
         )
+
+
+class Poem(models.Model):
+    author = models.ForeignKey(
+        'Poet', on_delete=models.CASCADE, verbose_name='Автор(ша)',
+        related_name='poems'
+    )
+    title = models.CharField('Название', max_length=250)
+    text = models.TextField('Текст')
+    year = models.CharField('Год(ы)', max_length=50, blank=True, default='')
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'Стихотворение'
+
+    def __str__(self):
+        return f'{self.author.full_name}: {self.title}'
--- auth_server/poems/tests/test_models.py
+++ auth_server/poems/tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ..models import Poet
+from ..models import Poem, Poet
 
 
 class TestPoet:
@@ -25,3 +25,22 @@ class TestPoet:
         )
 
         assert str(poet) == f'Поэт(эсса): {fname} {lname}'
+
+
+class TestPoem:
+    @pytest.mark.django_db
+    def test_str(self):
+        title = 'Гуляка'
+        fname = 'Сергей'
+        lname = 'Есенин'
+        poet = Poet.objects.create(
+            first_name=fname, last_name=lname
+        )
+
+        poem = Poem.objects.create(
+            author=poet,
+            title=title,
+            text='Я обманывать себя не стану'
+        )
+
+        assert str(poem) == f'{fname} {lname}: {title}'
