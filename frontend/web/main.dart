c1fd09a68d2e1be7e7cfc49965dd056a2369be72
Dastan Abd <dastand.climber@gmail.com>
2021-03-11 21:45:34 +0600

    feat: Finish get-started lesson

--- frontend/web/main.dart
+++ frontend/web/main.dart
@@ -1,5 +1,20 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  Element output = querySelector('#output');
+  output.children.addAll(thingsTodo().map(newLI));
+}
+
+LIElement newLI(String itemText) => LIElement()..text = itemText;
+
+Iterable<String> thingsTodo() sync* {
+  var actions = ['Walk', 'Wash', 'Feed'];
+  var pets = ['cats', 'dogs'];
+
+  for (var action in actions) {
+    for (var pet in pets) {
+      if (pet == 'cats' && action != 'Feed') continue;
+      yield '$action the $pet';
+    }
+  }
 }
