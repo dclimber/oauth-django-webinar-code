2effad628de1561def94ab3376606963c2ae7229
Dastan Abd <dastand.climber@gmail.com>
2021-03-12 23:58:59 +0600

    feat: Add dummy callback url

--- app_server/front/views.py
+++ app_server/front/views.py
@@ -1,8 +1,13 @@
-
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_GET
 
 
 def index(request):
     context = {
     }
     return render(request, 'front/index.html', context)
+
+
+@require_GET
+def callback(request):
+    return redirect('index')
