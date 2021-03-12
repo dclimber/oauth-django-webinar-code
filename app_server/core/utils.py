c2356e0aa408e521f09bfdd276844fe7fde5dc2a
Dastan Abd <dastand.climber@gmail.com>
2021-03-13 00:49:35 +0600

    fix: Remove trailing slash

--- app_server/core/utils.py
+++ app_server/core/utils.py
@@ -4,4 +4,4 @@ from django.urls import reverse
 def get_full_url(request, url_name):
     server_host = request.get_host()
     url = reverse(url_name)
-    return f'{request.scheme}://{server_host}{url}/'
+    return f'{request.scheme}://{server_host}{url}'
