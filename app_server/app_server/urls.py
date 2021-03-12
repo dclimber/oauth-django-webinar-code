3fb95a5c60ad93505b47b1b889269eac11346fae
Dastan Abd <dastand.climber@gmail.com>
2021-03-12 23:55:34 +0600

    feat: Add index view

--- app_server/app_server/urls.py
+++ app_server/app_server/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include('users.urls')),
+    path('', include('front.urls')),
 ]
