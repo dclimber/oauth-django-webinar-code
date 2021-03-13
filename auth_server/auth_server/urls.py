2e8cbec92056f9a980c06a442e00e1b531fc1f26
Dastan Abd <dastand.climber@gmail.com>
2021-03-13 14:55:18 +0600

    feat: Add index view with url list for convenience

--- auth_server/auth_server/urls.py
+++ auth_server/auth_server/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     # API
     path('api/', include('poems.urls')),
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
